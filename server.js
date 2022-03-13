f36fa3e505d2d7f481b0a9539d825aeb0530e1b3
EddieLo <eddarlostudy@yahoo.com>
2022-03-13 08:46:01 -0700

    fixing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
 const PORT = process.env.PORT || 3001;
-const app = express()
 const fs = require('fs');
 const path = require('path');
+
+const express = require('express');
+const app = express()
 
 const allNotes = require('./db/db.json');
 
